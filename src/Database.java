c838d45d3b9fdbc6fe986ebd2a466b79e2396a87
Karl Sixten Bjarnø <karlsixten@gmail.com>
2023-09-11 21:37:35 +0200

    Added interactive loop to Main.java Added toString() to Superhero.java Added ArrayList and addSuperHero() to Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,31 @@
-public class Database {
-    private Superhero[] superheroesList = new Superhero[5];
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public void addSuperhero(String superheroName, String realName, String superpower, int yearCreated, boolean isHuman, int strength, int listIndex) {
-        superheroesList[listIndex] = new Superhero(superheroName, realName, superpower, yearCreated, isHuman, strength);
+public class Database {
+    //Create scanner called input
+    Scanner input = new Scanner(System.in);
+    //Create new Arraylist with initial capacity of 1
+    private ArrayList<Superhero> superheroesArrayList = new ArrayList<Superhero>(1);
+
+    public void addSuperhero() {
+        //Get info about superhero to add
+        System.out.println("What is the superheros superhero name?");
+        String superheroName = input.nextLine();
+        System.out.println("What is the superheros real name?");
+        String realName = input.nextLine();
+        System.out.println("What is the superheros superpower?");
+        String superpower = input.nextLine();
+        System.out.println("When was this superhero created?");
+        int yearCreated = input.nextInt();
+        System.out.println("Is the superhero human? true/false");
+        boolean isHuman = input.nextBoolean();
+        System.out.println("What is the superheros strength? int");
+        int strength = input.nextInt();
+
+        //Creates a new superhero object and adds to index 0 of superheroesArrayList
+        superheroesArrayList.add(0, (new Superhero(superheroName, realName, superpower, yearCreated, isHuman, strength)));
+
+        //Test printlinje
+        System.out.println(superheroesArrayList.get(0));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //Create scanner called input
+        Scanner input = new Scanner(System.in);
+        //Initialise variable to check for users choice
+        int userChoice;
+        //Create new Database object
+        Database superheroDatabase = new Database();
 
+        //Introduction print lines
+        System.out.println("Welcome to the superhero universe!");
+
+        do {
+            System.out.println("1. Create superhero \n9. Exit.");
+            //Asks user what they want to do
+            userChoice = input.nextInt();
+            if (userChoice == 1) {
+                //Adds new superhero to database
+                superheroDatabase.addSuperhero();
+            }
+            else if (userChoice == 9) {
+                System.out.println("Exiting.");
+            }
+            else {
+                //Error message
+                System.out.println("Error");
+            }
+        }
+
+        while (userChoice != 9);
     }
 }
