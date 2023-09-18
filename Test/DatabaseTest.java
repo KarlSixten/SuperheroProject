06ca19cefd6ab13cf5172796ce385d71e685dff7
Aleksandergreg <alek3691@stud.kea.dk>
2023-09-18 16:21:47 +0200

    v0.1 - Added test method for addSuperhero

--- Test/DatabaseTest.java
+++ Test/DatabaseTest.java
@@ -2,12 +2,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
+    Superhero s1;
+    Superhero s2;
+    Database database;
 
     @BeforeEach
     void setUp() {
+        //arrange
+        database = new Database();
+        s1 = new Superhero("Spiderman", "Aner det ikke","Edderkoppesværd", 1990, true, 1000);
+        s2 = new Superhero("Ironman", "Vides ikke","Jern", 1980, true, 300);
+        //act
+        database.getSuperheroesArrayList().addAll(List.of(s1,s2));
+
     }
 
     @AfterEach
@@ -16,9 +28,17 @@ class DatabaseTest {
 
     @Test
     void getSearchMatches() {
+
     }
 
     @Test
+    //Tilføjer yderligere en superhelt udover de to hardcodede i databasen. Tester om expectedSize og størrelsen på min superheroesArraylist() er lige store.
     void addSuperhero() {
+        //Arrange
+        int expectedSize = 3;
+        //Act
+        database.addSuperhero("Holger", "Rune", "Tennisketsjer",2000, true, 7);
+        //assert
+        assertEquals(expectedSize, database.getSuperheroesArrayList().size());
     }
 }
