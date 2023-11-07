49247e731b1768c5a0152c2b155fc7c285b85f46
Karl Sixten Bjarnø <karlsixten@gmail.com>
2023-11-07 10:32:28 +0100

    Renaming of some variables in Comparators

--- src/comparator/SuperheroStrengthComparator.java
+++ src/comparator/SuperheroStrengthComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class SuperheroStrengthComparator implements Comparator <Superhero> {
     @Override
-    public int compare(Superhero o1, Superhero o2) {
-        return Integer.compare(o1.getStrength(), o2.getStrength());
+    public int compare(Superhero superhero1, Superhero superhero2) {
+        return Integer.compare(superhero1.getStrength(), superhero2.getStrength());
     }
 }
--- src/comparator/SuperheroYearCreatedComparator.java
+++ src/comparator/SuperheroYearCreatedComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class SuperheroYearCreatedComparator implements Comparator <Superhero> {
     @Override
-    public int compare(Superhero o1, Superhero o2) {
-        return Integer.compare(o1.getYearCreated(), o2.getYearCreated());
+    public int compare(Superhero superhero1, Superhero superhero2) {
+        return Integer.compare(superhero1.getYearCreated(), superhero2.getYearCreated());
     }
 }
